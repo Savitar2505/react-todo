42e0496783400626af12cb98fe102cc473a8fb77
Abdumalik Kochkorov <theabdyko@gmail.com>
2022-01-29 20:02:32 +0600

    Status filter functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,12 +11,13 @@ import './app.css';
 class App extends React.Component {
   state = {
       todos: [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 },
-        { label: 'Drink vodka', important: true, id: 4 },
-        { label: 'Drink mohito', important: false, id: 5 },
-      ]
+        { label: 'Drink Coffee', important: false, id: 1, done: false },
+        { label: 'Make Awesome App', important: true, id: 2, done: false },
+        { label: 'Have a lunch', important: false, id: 3, done: true },
+        { label: 'Drink vodka', important: true, id: 4, done: false },
+        { label: 'Drink mohito', important: false, id: 5, done: false },
+      ],
+      filter: 'active',
   }
 
   onDelete = (id) => {
@@ -51,19 +52,37 @@ class App extends React.Component {
     })
   }
 
+  onToggleFilter = (status) => {
+    this.setState({
+      filter: status
+    })
+  }
+
+  onStatusFilter = (todos, status) => {
+    if (status == 'active') {
+      return todos.filter((item) => item.done == false)
+    } else if (status == 'done') {
+      return todos.filter((item) => item.done == true)
+    } else {
+      return todos
+    }
+  }
+
   render() {
+    const filteredTodos = this.onStatusFilter(this.state.todos, this.state.filter)
+
     return (
       <div className="todo-app">
         <AppHeader toDo={1} done={3} />
         <div className="top-panel d-flex">
           <SearchPanel />
-          <ItemStatusFilter />
+          <ItemStatusFilter onToggleFilter={this.onToggleFilter} />
         </div>
 
         <TodoList
           onDelete={this.onDelete}
           onToggleImportant={this.onToggleImportant}
-          todos={this.state.todos}
+          todos={filteredTodos}
         />
       </div>
     );
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
+const ItemStatusFilter = ({onToggleFilter}) => {
   return (
     <div className="btn-group">
-      <button type="button"
+      <button onClick={() => onToggleFilter('all')}
+              type="button"
               className="btn btn-info">All</button>
-      <button type="button"
+      <button onClick={() => onToggleFilter('active')}
+              type="button"
               className="btn btn-outline-secondary">Active</button>
-      <button type="button"
+      <button onClick={() => onToggleFilter('done')}
+              type="button"
               className="btn btn-outline-secondary">Done</button>
     </div>
   );
